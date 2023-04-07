15f85ae39f9689d71859dcc7392191fd3d75911a
YouSafe <yelagabani@hotmail.com>
2023-04-07 06:57:34 +0200

    Take more samples in down-/upsamping shader

--- src/downsample.glsl
+++ src/downsample.glsl
@@ -14,6 +14,10 @@ layout(set = 0, binding = 1, rgba16f) writeonly uniform image2D outputImage;
 // output image has **HALF** the dimensions of the input image
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
+vec3 textureSample(vec2 uv, vec2 offset) {
+    return textureLod(inputImage, uv + offset * pass.texelSize, float(pass.mipLevel)).rgb;
+}
+
 void main() {
     ivec2 texel_output = ivec2(gl_GlobalInvocationID.xy);
     ivec2 texel_input = 2 * texel_output;
@@ -21,13 +25,54 @@ void main() {
     // uv coordinates between 0-1
     vec2 uv = vec2(texel_input) * pass.texelSize;
 
-    // one sample for now
-    vec3 e = textureLod(inputImage, uv, float(pass.mipLevel)).rgb;
+    // Source: https://learnopengl.com/Guest-Articles/2022/Phys.-Based-Bloom
+    // Take 13 samples around current texel:
+    // a - b - c
+    // - j - k -
+    // d - e - f
+    // - l - m -
+    // g - h - i
+    // === ('e' is the current texel) ===
 
-    vec3 downsample = e;
+    vec3 a = textureSample(uv, vec2(-2, 2));
+    vec3 b = textureSample(uv, vec2( 0, 2));
+    vec3 c = textureSample(uv, vec2( 2, 2));
 
-    // this should change the result image to be more green
-    downsample = vec3(0.0,1.0,0.0);
+    vec3 d = textureSample(uv, vec2(-2, 0));
+    vec3 e = textureSample(uv, vec2( 0, 0));
+    vec3 f = textureSample(uv, vec2( 2, 0));
+
+    vec3 g = textureSample(uv, vec2(-2,-2));
+    vec3 h = textureSample(uv, vec2( 0,-2));
+    vec3 i = textureSample(uv, vec2( 2,-2));
+
+    vec3 j = textureSample(uv, vec2(-1, 1));
+    vec3 k = textureSample(uv, vec2( 1, 1));
+    vec3 l = textureSample(uv, vec2(-1,-1));
+    vec3 m = textureSample(uv, vec2( 1,-1));
+
+    // code assumes that the coodinate system has its origin in the bottom left corner
+
+    // Apply weighted distribution:
+    // 0.5 + 0.125 + 0.125 + 0.125 + 0.125 = 1
+
+    // a,b,d,e * 0.125
+    // b,c,e,f * 0.125
+    // d,e,g,h * 0.125
+    // e,f,h,i * 0.125
+    // j,k,l,m * 0.5
+
+    // This shows 5 square areas that are being sampled. But some of them overlap,
+    // so to have an energy preserving downsample we need to make some adjustments.
+    // The weights are the distributed, so that the sum of j,k,l,m (e.g.)
+    // contribute 0.5 to the final color output. The code below is written
+    // to effectively yield this sum. We get:
+    // 0.125*5 + 0.03125*4 + 0.0625*4 = 1
+
+    vec3 downsample = e * 0.125
+        + (a+c+g+i) * 0.03125 // ((a+c+g+i) / 4) * 0.125
+        + (b+d+f+h) * 0.0625  // (b+d+f+h) / 2) * 0.125
+        + (j+k+l+m) * 0.125;  // (j+k+l+m) * 0.125
 
     imageStore(outputImage, texel_output, vec4(downsample, 1.0));
 }
--- src/upsample.glsl
+++ src/upsample.glsl
@@ -14,16 +14,43 @@ layout(set = 0, binding = 1, rgba16f) uniform image2D outputImage;
 // output image has **DOUBLE** the dimensions of the input image
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
+vec3 textureSample(vec2 uv, vec2 offset) {
+    return textureLod(inputImage, uv + offset * pass.texelSize, float(pass.mipLevel)).rgb;
+}
+
 void main() {
     ivec2 texel_output = ivec2(gl_GlobalInvocationID.xy);
     vec2 texel_input = vec2(texel_output) / 2.0;
 
     vec2 uv = texel_input * pass.texelSize;
 
-    // one sample for now
-    vec3 e = textureLod(inputImage, uv, float(pass.mipLevel)).rgb;
+    // Source: https://learnopengl.com/Guest-Articles/2022/Phys.-Based-Bloom
+    // Take 9 samples around current texel:
+    // a - b - c
+    // d - e - f
+    // g - h - i
+    // === ('e' is the current texel) ===
+    vec3 a = textureSample(uv, vec2(-1,1));
+    vec3 b = textureSample(uv, vec2(0,1));
+    vec3 c = textureSample(uv, vec2(1,1));
 
-    vec3 upsample = e;
+    vec3 d = textureSample(uv, vec2(-1,0));
+    vec3 e = textureSample(uv, vec2(0,0));
+    vec3 f = textureSample(uv, vec2(1,0));
+
+    vec3 g = textureSample(uv, vec2(-1,-1));
+    vec3 h = textureSample(uv, vec2(0,-1));
+    vec3 i = textureSample(uv, vec2(1,-1));
+
+    // Apply weighted distribution, by using a 3x3 tent filter:
+    //  1   | 1 2 1 |
+    // -- * | 2 4 2 |
+    // 16   | 1 2 1 |
+    vec3 upsample = 1.0 / 16.0 * (
+        4.0 * e +
+        2.0 * (b + d + f + h) +
+        1.0 * (a + c + g + i)
+    );
 
     // blend color additively with previous color
     upsample += imageLoad(outputImage, texel_output).rgb;
